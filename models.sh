49d01c523e58791337f0a3b0496676bf8e38aced
Akhil <166311327+Akhil-0911@users.noreply.github.com>
2025-03-24 21:03:51 +0530

    Update models.sh

--- models.sh
+++ models.sh
@@ -1,8 +1,8 @@
-Due to linited storage i have uploaded the model file into the google drive and pasted below:
+## Due to linited storage i have uploaded the model file into the google drive and pasted below:
 
 https://drive.google.com/drive/folders/1pT4hZ736b5UMR-zuJ0k5DasuiMZ-vS4O?usp=sharing
 
-Arrange the file in the given folder structure below
+## Arrange the file in the given folder structure below
 
 │── app.py  
 │── chest_xray_model.pth  
@@ -15,4 +15,4 @@ Arrange the file in the given folder structure below
 ├── templates/  
 │   ├── index.html  
 
-use the above folder structure ti arrage the files and folders for the proper execution
+## use the above folder structure ti arrage the files and folders for the proper execution
